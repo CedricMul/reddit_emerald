4a7b9563906b942a95c84fbc2adc8b7782173277
Cedric Mulvihill <cedric.mulvihill@gmail.com>
2020-10-13 15:28:15 -0400

    Switched the relationship between posts and subreddits so that now Subreddit has a many to many field for posts

--- Main/models.py
+++ Main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 #from User.models import RedditUser
+from Post.models import RedditPost
 
 class Subreddit(models.Model):
     name = models.CharField(max_length=80)
@@ -8,3 +9,9 @@ class Subreddit(models.Model):
     #     related_name='moderator',
     #     null=True
     # )
+    posts = models.ManyToManyField(
+        RedditPost,
+        related_name='sub_posts',
+        #null=True,
+        #blank=True
+    )
