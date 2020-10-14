9c2f6d2c1c0338049c5d3d6ea6f5bfce4ee2f8be
Cedric Mulvihill <cedric.mulvihill@gmail.com>
2020-10-14 13:57:52 -0400

    So called conflicting lines in main

--- Main/views.py
+++ Main/views.py
@@ -1,4 +1,5 @@
-
+from django.shortcuts import render, reverse, HttpResponseRedirect
+from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import DetailView
 
