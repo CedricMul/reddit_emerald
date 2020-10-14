9ce7c61f2fdb669f13f97f1eade2115399a16922
Cedric Mulvihill <cedric.mulvihill@gmail.com>
2020-10-14 10:30:40 -0400

    Copied index to main

--- Main/views.py
+++ Main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 
+from Main.models import Subreddit
+from Post.models import RedditPost
+
+class indexView(View):
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return render(request, "all.html", {"subreddits": RedditPost.objects.all().order_by('votes')})
+        user_subscriptions = request.user.subscriptions.all()
+        user_subs = RedditPost.objects.filter(id__in=[sub.id for sub in user_subscriptions])
+        return render(request, "all.html", {"subreddits": user_subs})
