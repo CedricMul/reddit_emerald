fd9ea13fc5f4775d5bfe6f52e72edd977d1ffc15
rhami223 <rhami223@gmail.com>
2020-10-14 12:53:30 -0400

    updated the upvote and downvotes and the sort view for main

--- Main/views.py
+++ Main/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 from Main.models import Subreddit
-
+from django.db.models import F
 class indexView(View):
     def get(self, request):
         return render(request, "index.html", {"subreddits": Subreddit.all()})
+
+def sort_view(request):
+    post = BoastRoast.objects.order_by(
+        -(F('upvotes') - F('downvotes'))
+    )
+    return render(request, 'sort.html', {'posts': post})
--- Post/views.py
+++ Post/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from .models import RedditPost
 # Create your views here.
+def upvotes_view(request, post_id):
+    post = RedditPost.objects.get(id=post_id)
+    post.upvotes = F('upvotes') + 1
+    post.save()
+    return HttpResponseRedirect(reverse('home'))
+
+def downvote_view(request, post_id):
+    post = RedditPost.objects.get(id=post_id)
+    post.downvotes = F('downvotes') + 1
+    post.save()
+    return HttpResponseRedirect(reverse('home'))
