0efd109e81de1950f836e87a4ccf24319fad0b72
Cedric Mulvihill <cedric.mulvihill@gmail.com>
2020-10-18 08:08:55 -0400

    Helper

--- Main/mod_helper.py
+++ Main/mod_helper.py
@@ -1,7 +1,8 @@
 from Main.models import Subreddit
+from User.models import RedditUser
 
-def is_mod(current_user, sub):
-    if Subreddit.objects.filter(subreddits_moderated__id=current_user.id):
+def is_mod(request, sub):
+    if request.user.subreddits_moderated.filter(id=sub.id):
         return True
     else:
         return False
