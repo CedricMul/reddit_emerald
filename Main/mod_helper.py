4b715e21715cc3bc4ef831450db21bce3a912caa
Cedric Mulvihill <cedric.mulvihill@gmail.com>
2020-10-18 07:09:26 -0400

    mod helper is working, updated subscription field for users so they aren't forced to subscribe

--- Main/mod_helper.py
+++ Main/mod_helper.py
@@ -1,5 +1,7 @@
+from Main.models import Subreddit
+
 def is_mod(current_user, sub):
-    if sub in current_user.subreddits_moderated:
+    if Subreddit.objects.filter(subreddits_moderated__id=current_user.id):
         return True
     else:
         return False
