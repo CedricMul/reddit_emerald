143d9372a0bd344be56f88027998ef4f3246413d
unknown <jrhuebottom676@gmail.com>
2020-10-19 18:13:19 -0400

    removed spaces in subreddit creation

--- Main/models.py
+++ Main/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 class Subreddit(models.Model):
-    name = models.CharField(max_length=80)
+    name = models.CharField(max_length=80, unique=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(Subreddit, self).save(*args, **kwargs)
+
+    def clean(self):
+        if self.name:
+            self.name = self.name.replace(' ', '')
